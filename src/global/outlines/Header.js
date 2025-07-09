0a3378b592b58d55bf9b56334bb92e887fcf0529
yonggyo1125 <yonggyo00@kakao.com>
2025-07-09 18:09:51 +0900

    프로젝트 시작

---
+++ src/global/outlines/Header.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import { MdHome, MdDraw, MdLogin } from 'react-icons/md';
+import color from '../styles/color';
+import fontsize from '../styles/fontsize';
+
+const { primary, light, dark } = color;
+const { big, extraBig } = fontsize;
+
+const StyledHeader = styled.header`
+  height: 90px;
+  display: flex;
+  background: ${primary};
+  color: ${light};
+  align-items: center;
+  padding: 0 20px;
+  justify-content: space-between;
+
+  .left,
+  .right {
+    display: flex;
+    flex-grow: 1;
+  }
+
+  .right {
+    justify-content: right;
+  }
+
+  a {
+    font-size: ${big};
+    color: ${light};
+    display: flex;
+    align-items: center;
+
+    &.on {
+      text-shadow: 2px 2px 5px ${dark};
+    }
+
+    svg {
+      font-size: ${extraBig};
+    }
+  }
+
+  a + a {
+    margin-left: 25px;
+  }
+`;
+
+const Header = () => {
+  return (
+    <StyledHeader className="layout-width">
+      <div className="left">
+        <NavLink to="/">
+          <MdHome />
+          <span>메인으로</span>
+        </NavLink>
+        <NavLink
+          to="/draw"
+          className={({ isActive }) => classNames({ on: isActive })}
+        >
+          <MdDraw />
+          <span>낙서하기</span>
+        </NavLink>
+      </div>
+      <div className="right">
+        <NavLink
+          to="/member/login"
+          className={({ isActive }) => classNames({ on: isActive })}
+        >
+          <MdLogin />
+          <span>로그인</span>
+        </NavLink>
+      </div>
+    </StyledHeader>
+  );
+};
+
+export default React.memo(Header);
---
+++ src/global/styles/fontsize.js
@@ -0,0 +1,10 @@
+const fontsize = {
+  extraBig: '3rem',
+  big: '2rem',
+  medium: '1.5rem',
+  normal: '1rem',
+  small: '0.85rem',
+  tiny: '0.7rem',
+};
+
+export default fontsize;
