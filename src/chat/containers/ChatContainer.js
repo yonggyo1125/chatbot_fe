89272cefc3dab8b0ea46469159c56234db8d6f0c
yonggyo1125 <yonggyo00@kakao.com>
2025-07-10 09:58:33 +0900

    .

--- src/chat/containers/ChatContainer.js
+++ src/chat/containers/ChatContainer.js
@@ -2,8 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import ChatLog from '../components/ChatLog';
 import ChatInput from '../components/ChatInput';
-
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  min-width: 320px;
+  max-width: 450px;
+  width: 100%;
+  margin: 20px auto;
+`;
 
 const ChatContainer = () => {
   return (
