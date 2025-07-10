c1b4fd5dca29908b09216357a1525f5a42c1358b
yonggyo1125 <yonggyo00@kakao.com>
2025-07-10 10:11:36 +0900

    .

--- src/chat/components/ChatInput.js
+++ src/chat/components/ChatInput.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaLocationArrow } from 'react-icons/fa';
+import color from '../../global/styles/color';
+import fontsize from '../../global/styles/fontsize';
+
+const { dark } = color;
+const { medium } = color;
+
+const StyledForm = styled.form`
+  display: flex;
+  height: 55px;
+  border: 3px solid ${dark};
+
+  button {
+    width: 50px;
+  }
+
+  input {
+    flex-grow: 1;
+    border: 0;
+    padding: 0 20px;
+    font-size: ${medium};
+    color: ${dark};
+  }
+`;
 
 const ChatInput = () => {
-  return <h1>채팅 입력..</h1>;
+  return (
+    <StyledForm>
+      <input type="text" name="message" placeholder="메세지를 입력하세요..." />
+      <button type="submit">
+        <FaLocationArrow />
+      </button>
+    </StyledForm>
+  );
 };
 
 export default React.memo(ChatInput);
--- src/chat/containers/ChatContainer.js
+++ src/chat/containers/ChatContainer.js
@@ -6,7 +6,10 @@ const Wrapper = styled.div`
   min-width: 320px;
   max-width: 450px;
   width: 100%;
+  min-height: 600px;
   margin: 20px auto;
+  display: flex;
+  flex-direction: column;
 `;
 
 const ChatContainer = () => {
